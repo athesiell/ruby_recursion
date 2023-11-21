d5bfc0245170d9e0504cd26be72b6f42725de124
Aidar <ar.ibraev98@gmail.com>
2023-11-21 19:34:09 +0600

    Update some changes

--- lib/ruby_recursion.rb
+++ lib/ruby_recursion.rb
@@ -2,7 +2,7 @@ def fibs(n)
   arr = [0,1]
   val = 2
   while val <= n
-    next_num = arr[val-1] + arr[val-2]
+    next_num = arr[val - 1] + arr[val - 2]
     arr.push(next_num)
     val += 1
   end
@@ -10,9 +10,9 @@ def fibs(n)
 end
 
 def fibs(n)
-  arr = [0,1]
+  arr = [0, 1]
   (2..n).each do |i|
-    arr.push(arr[i-1] + arr[i-2])
+    arr.push(arr[i - 1] + arr[i - 2])
   end
   arr
 end
@@ -23,8 +23,8 @@ def fibs_rec(n)
   elsif n == 1
     return [0, 1]
   else
-    arr = fibs_rec(n-1)
-    arr.push(arr[n-1] + arr[n-2])
+    arr = fibs_rec(n - 1)
+    arr.push(arr[n - 1] + arr[n - 2])
     return arr
   end
 end

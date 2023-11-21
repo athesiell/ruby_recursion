a4318cda4c85248bc1d84ea419f9598f7b5e86e4
Aidar <ar.ibraev98@gmail.com>
2023-11-21 15:49:20 +0600

    Add methods for Fibonacci numbers

---
+++ lib/ruby_recursion.rb
@@ -0,0 +1,30 @@
+def fibs(n)
+  arr = [0,1]
+  val = 2
+  while val <= n
+    next_num = arr[val-1] + arr[val-2]
+    arr.push(next_num)
+    val += 1
+  end
+    arr
+end
+
+def fibs(n)
+  arr = [0,1]
+  (2..n).each do |i|
+    arr.push(arr[i-1] + arr[i-2])
+  end
+  arr
+end
+
+def fibs_rec(n)
+  if n == 0
+    return [0]
+  elsif n == 1
+    return [0, 1]
+  else
+    arr = fibs_rec(n-1)
+    arr.push(arr[n-1] + arr[n-2])
+    return arr
+  end
+end
